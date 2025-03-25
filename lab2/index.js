edad7114840bd9d1180b01e28afc0cb52711c5af
SashaMakivchuk <160537750+SashaMakivchuk@users.noreply.github.com>
2025-03-25 12:32:58 +0200

    Delete lab2 directory

--- lab2/index.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const app = require("./scr/app");
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-});
