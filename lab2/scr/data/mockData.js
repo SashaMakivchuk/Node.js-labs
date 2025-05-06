247d8af6c9452facdba42ccf95565d493e220cdc
Sasha Make <sachka11231@gmail.com>
2025-05-06 11:47:43 +0300

    n

--- lab2/scr/data/mockData.js
+++
@@ -1,6 +0,0 @@
-let authors = [
-    {id:1, name: "name1", address:"addres1", topic: "topic1"},
-    {id:2, name: "name2", address:"addres2", topic: "topic2"},
-    {id:3, name: "name3", address:"addres3", topic: "topic3"},
-];
-module.exports = authors;
